8462f9e0938bb25fa2e475073fe6ab8094eb2d43
xuvictory <425723759@qq.com>
2026-07-29 15:00:41 +0800

    Lesson1 1，了解Wpf目录结构。 2，了解XAML的基本概念。 3，简单案例实操。

--- Lesson1/App.xaml.cs
+++ Lesson1/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Lesson1.案例1;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,21 @@ namespace Lesson1
     /// </summary>
     public partial class App : Application
     {
+        #region 案例1
+        //// 重写 OnStartup 方法，在应用程序启动时执行自定义逻辑
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    // 调用基类，确保触发 Startup 事件等
+        //    base.OnStartup(e);
+
+        //    // 此处可以添加启动前的初始化代码，例如检查配置、用户认证等
+
+        //    // 手动创建主窗口并显示
+        //    NewWindow newWindow = new NewWindow();
+        //    this.MainWindow = newWindow;// 这句话很关键，漏掉会有影响（猜猜到底会怎样？？？）
+        //    newWindow.Show();
+        //} 
+        #endregion
     }
 
 }
